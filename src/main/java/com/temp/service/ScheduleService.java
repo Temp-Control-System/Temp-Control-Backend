58f7f19a2fe56998d2ff661a3bdcc68968c577b3
zwd973 <1424437116@qq.com>
2020-05-28 16:02:41 +0800

    conflict fixed

--- src/main/java/com/temp/service/ScheduleService.java
+++ src/main/java/com/temp/service/ScheduleService.java
@@ -1,7 +1,32 @@
 package com.temp.service;
 
+import com.temp.domain.RoomStatus;
+import com.temp.schedule.ServiceUnitManager;
+import com.temp.SystemConfigure;
+import com.temp.domain.WindSupplyRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScheduleService {
+    @Autowired
+    private ServiceUnitManager serviceUnitManager  = null;
+
+    @Scheduled(fixedRate = SystemConfigure.timeSlot * 1000)
+    public void timeSlotSchedule(){
+        serviceUnitManager.executePerTimeSlot();
+    }
+
+    public void addRequest(int roomId, WindSupplyRequest wsr){
+        serviceUnitManager.handNewReq(roomId,wsr);
+    }
+
+    public void takeOff(int roomId){
+        serviceUnitManager.takeOff(roomId);
+    }
+
+    public RoomStatus getRoomStatus(int roomId){
+        return  serviceUnitManager.getRoomStatus(roomId);
+    }
 }
